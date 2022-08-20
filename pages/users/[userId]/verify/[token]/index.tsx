43e2182830e581611010c404595318f05e9f6a63
MarzanaBegum <marzanaBegum473@gmail.com>
2022-08-20 21:03:21 +0600

    email-verify-api-connect

--- pages/users/[userId]/verify/[token]/index.tsx
+++ pages/users/[userId]/verify/[token]/index.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import api from "./../../../../../api/index";
+import Link from "next/link";
 
-const index = () => {
+const Index = () => {
+  const router = useRouter();
+  const path = router.asPath.split("/");
+  const id = path[2];
+  const token = path[4];
+
+  useEffect(() => {
+    api.get(
+      `https://oda-center.herokuapp.com/api/signup/${id}/verify/${token}`
+    );
+  }, []);
+
   return (
-    <div>
-      <h3>Hello</h3>
+    <div className="success-bg">
+      <div className="w-[100%] xs:flex xs:justify-center xs:pt-[53.93px] sm:pl-[40px] sm:pt-[20px] xl:justify-start 3xl:pl-[180px] 3xl:pt-[60px]">
+        <img src="/img/logo.svg" alt="" className="xl:w-[230px] xs:w-[198px]" />
+      </div>
+      <div className="w-[100%] h-[100vh] flex justify-center items-center relative px-[4px]">
+        <div className="w-[639px] mx-auto text-center">
+          <img
+            src="/img/mail.svg"
+            alt="main icon"
+            className="w-[186px] h-[200px] mx-auto"
+          />
+          <h3 className="font-bold text-center lg:text-[50px] lg:leading-[60px] 3xl:text-[60px] 3xl:leading-[70px] xs:text-[24px] xs:leading-[32.68px] mt-[30px]">
+            Your email has been verified!
+          </h3>
+          <button className="w-[153px] h-[45px] rounded-[4px] bg-primary text-[16px] text-[white] mt-[40px]">
+            <Link href="/signin">Sign in</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default index;
+export default Index;
