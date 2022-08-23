9eca3942153f3ab5e79ba3a434e12da9efdf266d
AbdurRakib960 <rakibabdur960git@gmail.com>
2022-08-24 02:46:57 +0600

    shared-collection-page and content view collection view page done

--- components/CustomIcons/DownloadIcon.tsx
+++ components/CustomIcons/DownloadIcon.tsx
@@ -6,7 +6,7 @@ const DownloadIcon = (props:IconType) => {
     <CustomIcon
     {...props}
       svg={({stroke,color}) => (
-        <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="12" height="12" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 4.5V10H13V4.5M7 0V7M4 4.5L7 7.5L10 4.5" stroke={stroke}/>
         </svg>
         
--- components/CustomIcons/TagIcon.tsx
+++ components/CustomIcons/TagIcon.tsx
@@ -6,7 +6,7 @@ const TagIcon = (props:IconType) => {
     <CustomIcon
     {...props}
       svg={({stroke,color}) => (
-        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="12" height="12" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 6L1.5 1.5L6 1L13 8L8 13L1 6Z" stroke={stroke}/>
         <path d="M6 4.5C6 5.32843 5.32843 6 4.5 6C3.67157 6 3 5.32843 3 4.5C3 3.67157 3.67157 3 4.5 3C5.32843 3 6 3.67157 6 4.5Z" stroke={stroke}/>
         </svg>
