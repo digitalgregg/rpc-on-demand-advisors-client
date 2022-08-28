b600ed701402215e16229011519dc186fecd2ec6
MarzanaBegum <marzanaBegum473@gmail.com>
2022-08-28 21:55:17 +0600

    user-management-modal-update

--- utils/GlobalReactSelectData/AssetUse.ts
+++ utils/GlobalReactSelectData/AssetUse.ts
@@ -24,9 +24,36 @@ export const customStyles = {
   }),
 };
 
+
 export const options = [
   { value: "Demo text1", label: "Demo text1" },
   { value: "Demo text2", label: "Demo text2" },
   { value: "Demo text3", label: "Demo text3" },
   { value: "Demo text4", label: "Demo text4" },
 ];
+
+export const userManagementstyles = {
+  control: (base: any, state: any) => ({
+    ...base,
+    border: "1px solid #9E9E9E",
+    boxShadow: "none",
+    height: "55px"
+  }),
+  option: (provided: any) => ({
+    ...provided,
+    color: "#000000",
+    fontSize: "14px",
+    fontWeight: 400,
+    width: "95%",
+    
+    borderRadius: "4px",
+    margin: "0 auto",
+    "&:hover": {
+      color: "#E51937",
+      fontWeight: 600,
+    },
+  }),
+  indicatorsContainer: (provided: any) => ({
+    border: "none",
+  }),
+};
