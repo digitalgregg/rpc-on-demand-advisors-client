be4ddb4a1c029744f8300a79f0b091e96c06de2b
MarzanaBegum <marzanaBegum473@gmail.com>
2022-10-06 02:56:36 +0600

    design-issue-demo

--- components/ResourcesDemo/Increase/increaseSffectiveness.tsx
+++ components/ResourcesDemo/Increase/increaseSffectiveness.tsx
@@ -9,7 +9,7 @@ type Cardtype = {
 
 const Card = ({ number, title, p }: Cardtype) => {
     return (
-        <div className=" w-[360px] h-[289px]  md:h-[380px] lg:h-[289px] mx-auto overflow-hidden rounded bg-[#ffffff]">
+        <div className="w-[100%] xs:h-[300px] xl:h-[260px] 2xl:h-[289px] mx-auto overflow-hidden rounded bg-[#ffffff]">
             <div className=" relative transform -translate-x-[1px] -translate-y-[1px]">
                 <div className=" max-w-[86px]">
                     <Image
@@ -25,8 +25,8 @@ const Card = ({ number, title, p }: Cardtype) => {
             </div>
 
             <div className=" px-5 pt-[30px]">
-                <h3>{title}</h3>
-                <p>{p}</p>
+                <h3 className="text-[#000000] xs:font-bold xs:text-[16px] mb-[16px] xs:leading-[150%] sm:text-[18px] sm:leading-[25px] sm:font-semibold">{title}</h3>
+                <p className="text-[#4F4F4F] xs:text-[14px] font-normal xs:leading-[19px] md:text-[16px] md:leading-[22px]">{p}</p>
             </div>
         </div>
     );
@@ -38,17 +38,17 @@ const increaseSffectiveness = () => {
             <div className=" xl:p-0 lg:mx-[47px] md:mx-[50px] sm:mx-[40px] xs:mx-[20px] ">
                 <div className="!max-w-[1140px] mx-auto">
                     <div className="!max-w-[879px] mx-auto mb-10 px-8 md:px-0">
-                        <h2 className=" text-center mb-5">
+                        <h2 className="mb-5 text-center ">
                             Increase sales effectiveness
                         </h2>
-                        <p className=" text-center">
+                        <p className="text-center ">
                             Bring your content together in one place, enable
                             your sales team to have better conversations with
                             prospects, increase Marketing-Sales communication,
                             and prove the impact of content marketing.
                         </p>
                     </div>
-                    <div className=" flex gap-[30px] md:flex-row flex-col justify-center md:justify-between">
+                    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xs:grid-cols-1 gap-[24px] xs:justify-center sm:justify-between">
                         <Card
                             number="01"
                             title="Add and import content"
