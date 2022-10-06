5fe27017a48632b15c555bc54a7712639c368e00
AbdurRakib960 <rakibabdur960git@gmail.com>
2022-10-06 21:36:38 +0600

    next.config update and wisht list role updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,
-}
+    reactStrictMode: true,
+    swcMinify: false,
+    images: {
+        domains: ["rpc-s3-upload.s3.ap-south-1.amazonaws.com"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
